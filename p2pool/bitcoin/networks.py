997b321657672dee0a33272a7c0dc3ff65427427
haze-mobicow <w.gray@mobicow.com>
2014-01-08 17:32:00 -0500

    addd coinyecoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -178,6 +178,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=1e8,
     ),
+    coinyecoin=math.Object(
+            P2P_PREFIX='f9f7c0e8'.decode('hex'),
+            P2P_PORT=41338,
+            ADDRESS_VERSION=11,
+            RPC_PORT=41337,
+            RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+                'coinyecoinaddress' in (yield bitcoind.rpc_help()) and
+                not (yield bitcoind.rpc_getinfo())['testnet']
+            )),
+            SUBSIDY_FUNC=lambda height: 666666*100000000,
+            POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+            BLOCK_PERIOD=90, # s
+            SYMBOL='COYE',
+            CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'coinyecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Appl$
+            BLOCK_EXPLORER_URL_PREFIX='http://na.net:2750/block/',
+            ADDRESS_EXPLORER_URL_PREFIX='http://na.net:2750/address/',
+            TX_EXPLORER_URL_PREFIX='http://na:2750/tx/',
+            SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+            DUMB_SCRYPT_DIFF=2**16,
+            DUST_THRESHOLD=1e8,
+        ),
 
 )
 for net_name, net in nets.iteritems():
