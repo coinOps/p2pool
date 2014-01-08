0cfebccffb4000614a25c04d8c3fddbad50a4a20
haze-mobicow <w.gray@mobicow.com>
2014-01-08 18:25:54 -0500

    Added coinyecoin blockchain.info

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -192,9 +192,9 @@ nets = dict(
             BLOCK_PERIOD=90, # s
             SYMBOL='COYE',
             CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'coinyecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Appl$
-            BLOCK_EXPLORER_URL_PREFIX='http://na.net:2750/block/',
-            ADDRESS_EXPLORER_URL_PREFIX='http://na.net:2750/address/',
-            TX_EXPLORER_URL_PREFIX='http://na:2750/tx/',
+            BLOCK_EXPLORER_URL_PREFIX='http://coinyechain.info/block/',
+            ADDRESS_EXPLORER_URL_PREFIX='http://coinyechain.info/address/',
+            TX_EXPLORER_URL_PREFIX='http://coinyechain.info/tx/',
             SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
             DUMB_SCRYPT_DIFF=2**16,
             DUST_THRESHOLD=1e8,
